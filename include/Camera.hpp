5ed1493e0dd46d587ce7ce4f7809900976a455cd
Dimethylebutane <qwer5t.game@gmail.com>
2022-12-27 22:38:32 +0100

    Camera funcs in there own file

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -35,70 +35,27 @@ struct Camera
 
 	std::vector<VkDescriptorSet> m_descrptrSet;
 
-	void init(const unsigned char numberOfFrame)
-	{
-		assert(numberOfFrame != 0);
+	void init(const unsigned char numberOfFrame);
 
-		m_camBuffMem = createUBO<CamUBO>(numberOfFrame, CameraDevh);
-		m_descrptrSet = createDescriptorSets<CamUBO>(numberOfFrame, CameraDescriptorSetLayout, CameraDescriptorPool, CameraDevh.device, m_camBuffMem);
-	}
+	void clean();
 
-	void clean()
-	{
-		//destroy buffer and free memory
-		destroyUBO(m_camBuffMem, CameraDevh.device);
-		//free descriptorSets
-		vkFreeDescriptorSets(CameraDevh.device, CameraDescriptorPool, MAX_FRAMES_IN_FLIGHT, m_descrptrSet.data());
-	}
+	void updateBuffer(uint16_t imageIndex);
 
-	void updateBuffer(uint16_t imageIndex)
-	{
-		m_data.view = glm::lookAt(m_data.pos, m_data.front + m_data.pos, m_data.up);
-		
-		memcpy(m_camBuffMem.uniformBuffersMapped()[imageIndex], &m_data, sizeof(m_data));
-	}
+	void updateCamPosition(glm::vec3 newPos);
 
-	void updateCamPosition(glm::vec3 newPos)
-	{
-		m_data.pos = newPos;
-	}
+	void moveCameraOf(glm::vec3 deplacement);
 
-	void moveCameraOf(glm::vec3 deplacement)
-	{
-		m_data.pos += deplacement;
-	}
+	void lookAt(glm::vec3 Center);
 
-	void lookAt(glm::vec3 Center)
-	{
-		glm::vec3 old = m_data.front;
-
-		m_data.front = Center - m_data.pos;
-
-		m_data.up = glm::cross(m_data.front, m_data.up) == glm::vec3(0, 0, 0) ? -old : m_data.up;
-	}
-
-	void rotateCamera(glm::vec3 axis, float angle)
-	{
-		m_data.front = glm::rotate(glm::mat4(1), angle, axis) * glm::vec4(m_data.front, 1);
-		m_data.up = glm::rotate(glm::mat4(1), angle, axis) * glm::vec4(m_data.up, 1);
-	}
+	void rotateCamera(glm::vec3 axis, float angle);
 
 	//static stuff
 	static VkDescriptorPool CameraDescriptorPool;
 	static DeviceHandler CameraDevh;
 	static VkDescriptorSetLayout CameraDescriptorSetLayout;
 
-	static void InitCamerasClass(uint32_t NumberOfImageToBind, DeviceHandler devh)
-	{
-		CameraDevh = devh;
-		CameraDescriptorPool = createDescriptorPool(NumberOfImageToBind, CameraDevh.device, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT);
-		CameraDescriptorSetLayout = createDescriptorSetLayout(CameraDevh.device);
-	}
+	static void InitCamerasClass(uint32_t NumberOfImageToBind, DeviceHandler devh);
 
-	static void CleanCameraClass()
-	{
-		vkDestroyDescriptorPool(CameraDevh.device, CameraDescriptorPool, nullptr);
-		vkDestroyDescriptorSetLayout(CameraDevh.device, CameraDescriptorSetLayout, nullptr);
-	}
+	static void CleanCameraClass();
 };
 #endif
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,5 +1,66 @@
 #include "../include/Camera.hpp"
 
+void Camera::init(const unsigned char numberOfFrame)
+{
+	assert(numberOfFrame != 0);
+
+	m_camBuffMem = createUBO<CamUBO>(numberOfFrame, CameraDevh);
+	m_descrptrSet = createDescriptorSets<CamUBO>(numberOfFrame, CameraDescriptorSetLayout, CameraDescriptorPool, CameraDevh.device, m_camBuffMem);
+}
+
+void Camera::clean()
+{
+	//destroy buffer and free memory
+	destroyUBO(m_camBuffMem, CameraDevh.device);
+	//free descriptorSets
+	vkFreeDescriptorSets(CameraDevh.device, CameraDescriptorPool, MAX_FRAMES_IN_FLIGHT, m_descrptrSet.data());
+}
+
+void Camera::updateBuffer(uint16_t imageIndex)
+{
+	m_data.view = glm::lookAt(m_data.pos, m_data.front + m_data.pos, m_data.up);
+
+	memcpy(m_camBuffMem.uniformBuffersMapped()[imageIndex], &m_data, sizeof(m_data));
+}
+
+void Camera::updateCamPosition(glm::vec3 newPos)
+{
+	m_data.pos = newPos;
+}
+
+void Camera::moveCameraOf(glm::vec3 deplacement)
+{
+	m_data.pos += deplacement;
+}
+
+void Camera::lookAt(glm::vec3 Center)
+{
+	glm::vec3 old = m_data.front;
+
+	m_data.front = Center - m_data.pos;
+
+	m_data.up = glm::cross(m_data.front, m_data.up) == glm::vec3(0, 0, 0) ? -old : m_data.up;
+}
+
+void Camera::rotateCamera(glm::vec3 axis, float angle)
+{
+	m_data.front = glm::rotate(glm::mat4(1), angle, axis) * glm::vec4(m_data.front, 1);
+	m_data.up = glm::rotate(glm::mat4(1), angle, axis) * glm::vec4(m_data.up, 1);
+}
+
 VkDescriptorPool Camera::CameraDescriptorPool{};
 DeviceHandler Camera::CameraDevh{};
 VkDescriptorSetLayout Camera::CameraDescriptorSetLayout{};
+
+void Camera::InitCamerasClass(uint32_t NumberOfImageToBind, DeviceHandler devh)
+{
+	CameraDevh = devh;
+	CameraDescriptorPool = createDescriptorPool(NumberOfImageToBind, CameraDevh.device, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT);
+	CameraDescriptorSetLayout = createDescriptorSetLayout(CameraDevh.device);
+}
+
+void Camera::CleanCameraClass()
+{
+	vkDestroyDescriptorPool(CameraDevh.device, CameraDescriptorPool, nullptr);
+	vkDestroyDescriptorSetLayout(CameraDevh.device, CameraDescriptorSetLayout, nullptr);
+}
