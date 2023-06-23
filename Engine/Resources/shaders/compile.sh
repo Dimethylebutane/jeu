46d6d9125307cd4a98e794895615f418364260fd
Grégoire <gregoire.dreno@gadz.org>
2023-06-23 21:30:22 +0200

    test

--- Engine/Resources/shaders/compile.sh
+++ Engine/Resources/shaders/compile.sh
@@ -1,25 +1,23 @@
-@echo off
-set output=%1
-set vulkPath=%2
-
+#!/bin/zsh
+output=%1
 echo Compiling shaders...
 
-:: compile all {name}.vert and {name}.frag file in the same folder
-:: output result in output/{name}.spv
-:: get vulkan path in argument to find glsl compiler
+#compile all {name}.vert and {name}.frag file in the same folder
+#output result in output/{name}.spv
+#get vulkan path in argument to find glsl compiler
 
-set /A N=0
-if not exist %output% mkdir %output%
-if not exist %output%\shaders mkdir %output%\shaders
+mkdir -p $output
 
-for /f %%f IN ('dir /b *.vert') do (
-    start %vulkPath%\Bin\glslc.exe %%f -o %output%\%%~nf.spv
-    set /a N+=1
-)
+FILES=$(pwd)
 
-for /f %%f IN ('dir /b *.frag') do (
-    start %vulkPath%\Bin\glslc.exe %%f -o %output%\%%~nf.spv
-    set /a N+=1
-)
+N=0
 
-echo %N% shaders compiled
+for f in $FILES
+do
+    if [[ $file == *.frag ]] || [[ $file == *.vert ]]; then
+		glslc $f -o $output/${filename%.*}.spv
+		((N+=1))
+    fi
+done
+
+echo $N shaders compiled
